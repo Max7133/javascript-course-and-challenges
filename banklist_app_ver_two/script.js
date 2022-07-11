5e9db398ccdab1aefd03423196a21ab14ad94e15
Maksim Pasnitsenko <emexpas@outlook.com>
2022-07-11 20:44:35 +0300

    added current date in the withrawals/deposits and when logged in

--- banklist_app_ver_two/script.js
+++ banklist_app_ver_two/script.js
@@ -81,19 +81,35 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+// const displayMovements = function (movements, sort = false)
+// Passing the Entire Account not just 'movemenets'
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    // [i] - current Index in the 'movements' Array, and it's the same Index that going to point to the equivalent Date in this 'movementsDate' Array
+    // common techinique of looping over 2 Arrays at the same time
+    // new Date for nicely formatted time String, and I use this String for creating a New Date Object
+    // I need that Object so that then from there, I can call the usual Methods to get the Date, Month, Year
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0); // it's 0 based, so I add 1 here
+    const year = date.getFullYear();
+
+    // will display Date in the HTML in every withdrawal, deposit ('movements' Array)
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -142,7 +158,8 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  // displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,6 +171,11 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// Faked LOGGED IN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -170,6 +192,19 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    // Create Current Date and Time
+    // day/month/year, XX:XX
+    const now = new Date();
+    // adding 0 before date Number with padStart Method
+    // `${now.getDate()}` is now a String, .padStart(2, 0) for displaying only 2 Digits, and Padding with '0' if the day is less than 10
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0); // it's 0 based, so I add 1 here
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+    // building a String from that
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -198,6 +233,10 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // Add transfer Date
+    currentAccount.movementsDates.push(new Date().toISOString()); // it will push New Current Date in each new deposit/withdrawal
+    receiverAcc.movementsDates.push(new Date().toISOString()); // toISOString() Very useful case is when I want to Convert a particular Date Object into a String that I can then store somewhere
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -213,6 +252,9 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    // Add loan Date
+    currentAccount.movementsDates.push(new Date().toISOString()); // toISOString() Very useful case is when I want to Convert a particular Date Object into a String that I can then store somewhere
 
     // Update UI
     updateUI(currentAccount);
