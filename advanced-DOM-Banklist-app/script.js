d210b1eb55f0bec5d276208519e148299c788d50
Maksim Pasnitsenko <emexpas@outlook.com>
2022-07-15 20:06:27 +0300

    starting advanced DOM and events with Project Banklist Website

---
+++ advanced-DOM-Banklist-app/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// btnsOpenModal - Node list because it's the result of querySelectorAll
+// Node list is NOT an Array, but it still does have default forEach Method
+// Node list dosen't have most of the Methods that Arrays have
+// New forEach Method way
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// Old for Loop way
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
