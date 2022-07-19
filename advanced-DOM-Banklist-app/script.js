e193e6a9339f835ea6b8678d7c52090f122f8978
Maksim Pasnitsenko <emexpas@outlook.com>
2022-07-19 17:32:18 +0300

    Bankist website: implemented tabbed component

--- advanced-DOM-Banklist-app/script.js
+++ advanced-DOM-Banklist-app/script.js
@@ -110,3 +110,60 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   // Instead, I edit one big Event Handler Function to the Parent Element of all the Elements that I intrested in, and then I determined where the Click Event came from.
   // And then I needed MATCHING STRATEGY, because I wanted to IGNORE CLICKS that DID NOT HAPPEN right on one of the LINKS.
 });
+
+/////////////////////////////////////////////////////////
+////// Tabbed component
+
+// Selecting the Tabs
+const tabs = document.querySelectorAll('.operations__tab');
+// Selecting the Tabs Container
+const tabsContainer = document.querySelector('.operations__tab-container');
+// Selecting 3 Contant Areas
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//// Adding Event Handlers to the Buttons
+
+// BAD PRACTISE WAY (What if I had 200 Tabs ? Then I would have 200 copies of THIS EXACT Callback Function here, in result will Slow Down the Web Page)
+//tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+// PROPER WAY (With Event Delegation)
+// For Event Delegation, I need to attach the Event Handler on the Common Parent Element of all the Elements that I'm intrested in. (this case Tabs Container)
+tabsContainer.addEventListener('click', function (e) {
+  // Matching Strategy
+  // Figuring our which Button was clicked
+  // e.target will find the Button Element after the Click, but if I Click on the Number on the Button, it will show the Target of the SPAN Element, that I don't need
+  // to FIX it, I need to add .parentElement, I can then move UP the DOM Tree, so whenever I click it will give me the Button, that's because that is the Parent Element
+  //const clicked = e.target.parentElement; // FIX with Dom Traversing (because that's the Parent Element of that <span></span> )
+  //console.log(clicked); // <button class="btn operations__tab operations__tab--2" data-tab="2">...</button>
+  // BUT NOW, when I Click on the Button itself, I get, I will get the Parent of the Button Element, which is the Container, and that is something I don't want
+  // PROPER FIX
+  // What I want, is to get THE BUTTON, is NO MATTER if I Click on the SPAN, or on the Button itself, so I need a way of selecting the Parent Element that is ALWAYS A TAB.
+  // to FIX This AGAIN, I need to use the Closest Method instead of the '.parentElement'
+  // I want to go Upwards, but I want to specify that I want to select an Operations Tab, so the .closest() is for exactly that
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // FIX so I wont get NULL if I Click somewhere where there is gonna be No Parrent with this class ('.operations__tab) because of the closest()
+  // Guard clause - IF Statement which will Return EARLY if some condition is matched
+  // In this case if there is NOTHING Clicked than I want to immediately finish this Function (In this case, when I have NULL) which is Falsy and then !clicked NOT FALSY will become TRUE
+  // and then the Function will Return, and none of the code that's after it will be executed.
+  if (!clicked) return; // retuns immediatelly
+
+  // Remove active Classes for Tab & Content Areas
+  // in forEach it will remove the 'active' Class that lifts the Button Up when it's selected, therefore it will go down to it's default place
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  // Removing the Active Class for all of them before adding it to the one that I'm intrested in
+  tabsContent.forEach(con =>
+    con.classList.remove('operations__content--active')
+  );
+
+  //// Activate Tab
+  clicked.classList.add('operations__tab--active'); // will lift the Button Up
+
+  //// Activate Content Area
+  console.log(clicked.dataset.tab); // 2 if Button 2/3 clicked, 1 if Button 1/3 clicked
+  // Every time I Click a Button, that Button is stored in the Clicked Variable
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`) // clicked.dataset.tab shows Number 1 or 2 or 3, depending on which button I clicked
+    .classList.add('operations__content--active');
+});
